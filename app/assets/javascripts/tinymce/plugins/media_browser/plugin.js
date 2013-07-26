e4c2e3ea058b983e1887195104d2b3972f7e7622
sakomee <somee12@hotmail.com>
2013-07-26 11:35:45 +0300

    change image browser to media browser

--- app/assets/javascripts/tinymce/plugins/media_browser/plugin.js
+++ app/assets/javascripts/tinymce/plugins/media_browser/plugin.js
@@ -1,16 +1,15 @@
-tinymce.PluginManager.add('image_browser', function(editor, url) {
+tinymce.PluginManager.add('media_browser', function(editor, url) {
     // Add a button that opens a window
-    editor.addButton('image_browser', {
-        text: 'Insert image',
+    editor.addButton('media_browser', {
+        text: 'Insert media',
         icon: false,
-        onclick: open_image_browser        
+        onclick: open_media_browser        
     });    
     
     // Opens browser window
-    function open_image_browser() {
+    function open_media_browser() {
         editor.windowManager.open({
-            title: 'Image Manager',
-            id: 'file_manager',
+            title: 'media Manager',
             url: '/filemanagers/',
             width: 800,
             height: 600,
@@ -22,12 +21,12 @@ tinymce.PluginManager.add('image_browser', function(editor, url) {
                 text: 'Close',
                 onclick: 'close'
             }],
-            onsubmit: insert_images           
+            onsubmit: insert_media           
         });
     }
    
-    // Finds and inserts selected images 
-   function insert_images(){
+    // Finds and inserts selected media 
+   function insert_media(){
         var content=document.getElementsByClassName('mce-floatpanel')[0]
         var iframe=content.getElementsByTagName('iframe')[0]
         var body=iframe.contentWindow.document.body;
@@ -35,8 +34,8 @@ tinymce.PluginManager.add('image_browser', function(editor, url) {
         var range = editor.selection.getRng();
         var len=selected_img.length;  
         for(var i=0; i<len ; i++){
-            image_node=selected_img[i].getElementsByClassName('media')[0]
-            range.insertNode(image_node);  
+            media_node=selected_img[i].getElementsByClassName('media')[0]
+            range.insertNode(media_node);  
         }
     }
 });
